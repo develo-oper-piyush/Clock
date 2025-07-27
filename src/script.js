050ffe839553e2517c1860207c38aec2411e9180
Piyush Chaudhary <166219915+develo-oper-piyush@users.noreply.github.com>
2025-07-27 14:18:15 +0530

    Adding files

---
+++ src/script.js
@@ -0,0 +1,40 @@
+let hour = document.querySelector("#hour");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform = `rotate(${mrotation}deg)`;
+    sec.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+
+    let am_pm = htime > 12 ? "pm" : "am";
+
+    if (Number(htime) > 12) htime -= 12;
+    else if (Number(htime == 0)) htime = "12";
+    if (Number(mtime) / 10 < 1) mtime = "0" + mtime;
+
+    if (htime > 12) {
+        document.querySelector("#digital_clock").textContent = `${
+            htime - 12
+        } : ${mtime} ${am_pm}`;
+    } else {
+        document.querySelector(
+            "#digital_clock"
+        ).textContent = `${htime} : ${mtime} ${am_pm}`;
+    }
+}, 1000);
